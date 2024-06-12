208247a198fc04da962371df75dc9491f9e99065
junwon-hwnag <nyong1320@gmail.com>
2024-06-12 11:00:51 +0900

    # react props1

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import ExpenseItem from './components/Expenses/ExpenseItem';
 
 const App = () => {
 
+  // 속성 보낼때 {} 넣어야함 문자열은 생략가능
   return (
     <>
-      <ExpenseItem />
-      <ExpenseItem />
+      <ExpenseItem title={"치킨먹음"} price={30000} date={new Date(2024, 6, 3)}/>
+      <ExpenseItem title="족발먹음" price={40000} date={new Date(2024, 6, 7)}/>
+      <ExpenseItem title="헬스장등록" price={300000} date={new Date(2024, 6, 12)}/>
     </>
 
   );

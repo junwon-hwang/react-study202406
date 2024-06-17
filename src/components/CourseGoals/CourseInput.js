f55d8305f1a970573521e5987b7c5661a2d2871f
junwon-hwnag <nyong1320@gmail.com>
2024-06-17 10:34:49 +0900

    # styled components , css module # Fragment

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -1,8 +1,35 @@
 import React, { useState } from 'react';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 import Button from '../UI/Button';
 
+
+const foo = () => {
+
+  const arr = [1,2,3,4,5,6,700000000];
+
+  // 렌더링이 된 요소
+  const $ul = document.querySelector('ul.abc');
+
+  // 중간 가상 DOM 생성
+  const $div = document.createDocumentFragment();
+
+  arr.forEach(n => {
+    // 아직 렌더링이 되지 않은 요소 : virtual DOM
+    const $li = document.createElement('li');
+    $li.textContent = n;
+    // 렌더링된 곳에 렌더링되지않은 요소를 추가 : 성능상 좋지 않다.
+    $div.appendChild($li);
+  });
+
+  $ul.appendChild($div);
+};
+
 const CourseInput = ({onAdd}) => {
+
+  // css에 담긴 className값
+  // console.log(styles);
+
+  const { invalid, "form-control" : formControl } = styles;
 
   // 목표 인풋에 입력한 값
   const [enteredText, setEnteredText] = useState('');
@@ -43,18 +70,17 @@ const CourseInput = ({onAdd}) => {
 
 
 
+  // &&
+  // 왼쪽이 ture면 오른쪽 것으로 사용해라
+  // 왼쪽이 false면 오른쪽 무시해라
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`${formControl} ${!isValid ? invalid : ""}`}>
         <label>나의 목표</label>
         <input type="text" 
                onChange={goalChangeHandler} 
-               value={enteredText}
-               style={{
-                backgroundColor : isValid ? 'transparent' : 'salmon',
-                borderColor : isValid ? 'black' : 'red',
-               }}
-        />
+               value={enteredText}/>
       </div>
       <Button type="submit">목표 추가하기</Button>
     </form>

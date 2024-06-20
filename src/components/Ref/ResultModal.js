491cef73cc5d8b39c58109fc783381df8affec92
junwon-hwnag <nyong1320@gmail.com>
2024-06-20 12:28:31 +0900

    # 승패 조건

--- src/components/Ref/ResultModal.js
+++ src/components/Ref/ResultModal.js
@@ -1,17 +1,22 @@
 import React, { forwardRef } from 'react';
 
-const ResultModalComponent = ({result, targetTime,remainingTime}, ref) => {
-  
+const ResultModalComponent = ({onReset, result, targetTime,remainingTime}, ref) => {
+
+  // 패배 조건
+  const isLost = remainingTime <= 0 ;
+
+  // 시간 포맷팅
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   return (
     // dialog modal의 모든 함수를 제공함
     //    <form method="dialog"> 모달을 닫아주는 기능
     <dialog ref={ref} className="result-modal">
-      <h2>Your {result}!</h2>
+      {isLost && <h2>Your lost!</h2>}
       <p>The target time was <strong>{targetTime} seconds.</strong></p>
-      <p>You stopped the timer with <strong>{remainingTime} seconds left.</strong></p>
+      <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
       <form method="dialog">
-        <button>Close</button>
+        <button onClick={onReset}>Close</button>
       </form>
     </dialog>
   );

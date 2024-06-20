5bc44c0c07aec0368918ed012245beccbb0559a3
junwon-hwnag <nyong1320@gmail.com>
2024-06-20 11:34:43 +0900

    # dialog 태그 제어하기

---
+++ src/components/Ref/ResultModal.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from 'react';
+
+const ResultModal = ({result, targetTime}) => {
+
+
+  const dialog = useRef();
+
+  useEffect(()=>{
+    
+    dialog.current.showModal();
+  
+  },[]);
+
+  return (
+    // dialog modal의 모든 함수를 제공함
+    //    <form method="dialog"> 모달을 닫아주는 기능
+    <dialog ref={dialog} className="result-modal">
+      <h2>Your {result}!</h2>
+      <p>The target time was <strong>{targetTime} seconds.</strong></p>
+      <p>You stopped the timer with <strong>X seconds left.</strong></p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+};
+
+export default ResultModal;
--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -1,42 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
 
-const TimerChallenge = ({title,targetTime}) => {
- 
+const TimerChallenge = ({ title, targetTime }) => {
+  // timer를 ref변수로 관리 (컴포넌트 안에 값을 유지하는데 사용)
+  // 상태변수는 사용하면 렌더링에 영향을 줌
+  const timer = useRef();
+
   // 타이머가 시작되었는지를 확인하는 상태값
   const [timerStarted, setTimerStarted] = useState(false);
-
 
   // 타겟 시간이 종료되었는지 여부
   const [timerExpired, setTimerExpired] = useState(false);
 
-
-  const startHandler = e => {
-
-    setTimeout(()=>{
+  const startHandler = (e) => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
-    },targetTime * 1000)
+    }, targetTime * 1000);
 
-    setTimerStarted(true);
-  }
+      setTimerStarted(true);
+  };
 
+  // 이 상황에서 stop이 작동하지 않는 이유는
+  // start시의 timer 변수가 지역변수이기 때문에
+  // 상태변수의 setter 호출시 리렌더링이 되면서 새로운 지역변수로 바뀜
+  // stop시의 timer와 start시의 timer는 다른 변수다.
 
- 
+  // 전역변수로 timer 설정시 5초->1초->1초->5초를 연속 클릭해보면
+  // 5초짜리 timer가 정상작동하지 않는 이유는
+  // 4개의 TimeChallenge컴포넌트가 1개의 timer를 공유하여
+  // 처음 5초짜리 timer가 1초짜리에 의해 덮어씌워지기 때문
+
+  const stopHandler = (e) => {
+    clearTimeout(timer.current);
+  };
+
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>You lost!</p>}
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? 's' : ''}
-      </p>
-      <p>
-        <button onClick={startHandler}>
-          {timerStarted ? 'Stop' : 'Start'} Challenge
-        </button>
-      </p>
-      <p className={timerStarted? 'active' : undefined}>
-        {timerStarted ? 'Time is running...' :  'Timer inactive'}
-      </p>
-    </section>
+    <>
+      {<ResultModal targetTime={targetTime} result='lost'/>}
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerStarted ? stopHandler : startHandler}>
+            {timerStarted ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? "active" : undefined}>
+          {timerStarted ? "Time is running..." : "Timer inactive"}
+        </p>
+      </section>
+    </>
   );
 };
 

a14cd1dce73759a12bc4e8428e9e8e2144248f2c
junwon-hwnag <nyong1320@gmail.com>
2024-07-03 10:45:58 +0900

    # loader 기초 사용법

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -1,31 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import EventList from "../components/EventList";
-import EventsNavigation from "../layout/EventNavigation";
+import { useLoaderData } from "react-router-dom";
+
 
 const Events = () => {
-  const [eventList, setEventList] = useState([]);
 
-  // useEffect 안에 넣어 단 한번만 실행하도록 함 
-  // 아니면 게속해서 sever에 fetch를 보냄
-  useEffect(() => {
-
-    // async , await 사용법
-    (async () =>{
-      const response = await fetch("http://localhost:8282/events")
-      const jsonData = await response.json();
-      setEventList(jsonData);
-    })();
-
-    // than절
-    // fetch("http://localhost:8282/events")
-    //   .then((res) => res.json())
-    //   .then((jsonData) => {
-    //     console.log(jsonData);
-    //     setEventList(jsonData);
-    //   });
-
-  }, []);
+  // loader가 리턴한 데이터 받아오기
+  const eventList = useLoaderData();
 
   return (
     <>

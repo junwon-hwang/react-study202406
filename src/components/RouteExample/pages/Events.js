6e5bccb4d4419ee497f694a379c63b4e184a99f0
junwon-hwnag <nyong1320@gmail.com>
2024-07-10 09:19:00 +0900

    # 스켈레톤 스크린 개수 제어

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -26,6 +26,9 @@ const Events = () => {
   // 더이상 가져올 데이터가 있는지 확인
   const [isFinish, setIsFinish] = useState(false);
 
+  // 로딩 스켈레톤 스크린을 보여줄 개수
+  const [skeletonCount, setSkeletonCount] = useState(4);
+
   // 서버로 목록 조회 요청보내기
   const loadEvents = async () => {
 
@@ -50,6 +53,13 @@ const Events = () => {
 
     // 로딩이 끝나면 더이상 가져올게 있는지 여부를 체크한다.
     setIsFinish(totalCount===updatedEvents.length);
+
+    // 로딩 후 지금까지 불러온 데이터 개수(현재 렌더링된 개수)를 총 데이터 개수에서 차감
+    const restEventsCount = totalCount - updatedEvents.length;
+
+    // skeleton 개수 구하기 -> 남은 개수가 4보다 크면 4 4보다 작으면 그 수로 셋팅
+    const skeletonCnt = Math.min(4,restEventsCount);
+    setSkeletonCount(skeletonCnt);
 
   };
 
@@ -84,7 +94,7 @@ const Events = () => {
 
     <>
       <EventList eventList={events} />
-      {loading && <EventSkeleton />}
+      {loading && <EventSkeleton count={skeletonCount} />}
     </>
   );
 };
